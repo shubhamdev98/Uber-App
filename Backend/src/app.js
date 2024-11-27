d123ffe44de4de04ef41f65aa31d2d9b1f3f02a2
shubhamdev98 <shubham.pn.1998@gmail.com>
2024-11-27 22:48:19 +0530

    initial commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/v1/user', require('../src/router/user.routes'))
 
